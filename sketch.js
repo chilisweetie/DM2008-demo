de9eb544ebdc0c44d3485f62db5d3bd4e421660f
chilisweetie <59580954+chilisweetie@users.noreply.github.com>
2025-09-22 14:40:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,31 @@
-let radiusA = 20;
-let radiusB = 40;
+let radiusA = 80;
+let radiusB = 150;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
   
-  let d = dist(mouseX, mouseY, 200,200);
+  let d = dist(mouseX, mouseY, windowWidth/2,windowHeight/2);
   
   if (d < radiusA + radiusB){
     fill(0);
   } else {
     fill(255);
   }
-  ellipse(200,200,radiusA *2);
+  ellipse(windowWidth/2,windowHeight/2,radiusA *2);
   ellipse(mouseX,mouseY,radiusB *2);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function keyPressed(){
+  if (key === "f" || key === "F"){
+    let fs = fullscreen();
+    fullscreen(!fs);
+    }
 }
